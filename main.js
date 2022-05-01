5122102989b7a286435608051a27e15976c95954
yihanliao <yihanliao@local>
2022-05-01 11:08:21 +0800

    0430

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ const knex = require('knex')(config.db);
 const today = new Date();
 // let thisYear = 2021;
 let thisYear = today.getFullYear();
-// let date_month = 0; // 月份要 -1 別忘記 (五月要填4)
-let date_month = today.getMonth();
-// let date_days = [22,23];
-let date_days = [today.getDate() - 1];
+let date_month = 3; // 月份要 -1 別忘記 (五月要填4)
+// let date_month = today.getMonth();
+let date_days = [30];
+// let date_days = [today.getDate() - 1];
 
 /*
   前置準備
