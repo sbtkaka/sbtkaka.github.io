82282e3c1120c973e958f62099ecc93644bfaee4
Yihan <jili88tech@gmail.com>
2025-10-27 14:37:45 +0800

    ~1026

--- manual.js
+++ manual.js
@@ -3,7 +3,7 @@ const format = require("date-fns/format");
 const updateDailyReport = require(path.resolve("server", "updateDailyReport"));
 const { process } = require("./server/updateDailyReport/process");
 
-let targetDate = new Date(2025,8,3);
+let targetDate = new Date(2025,9,26);
 
 updateDailyReport(format(targetDate, 'yyyy-MM-dd'))
 .then(() => {
